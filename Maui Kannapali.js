aa150672b1eea6f8547ce0991f34fb518c683fbf
CuddleCatsGames <144502473+CuddleCatsGames@users.noreply.github.com>
2023-10-18 15:24:56 +0800

    Update and rename Maui Kannapali to Maui Kannapali.js

---
+++ Maui Kannapali.js
@@ -0,0 +1,227 @@
+// Maui Kaanapali Villas
+const mauiKaanapali = {
+	name: 'Maui Kaanapali Villas',
+	type: 'Resort',
+	homepage: 'https://www.astonmauikaanapalivillas.com/',
+	description: 'The Aston Maui Kaanapali Villas is set on 11 acres of exotic tropical gardens in the Kaanapali Resort area on one of the finest stretches of Kaanapali Beach.',
+	petsAllowedTrueFalse: false,
+	totalRoomCount: 187,
+	priceRange: '$$$',
+	amenitiesArr: [
+		{
+			name: 'Jump Right In',
+			url: 'https://www.astonmauikaanapalivillas.com/featured-amenities/',
+			imageUrl: 'https://s40340.pcdn.co/wp-content/uploads/2016/02/MKV-Pool-Aerial-495x400.jpg'
+		},
+		{
+			name: 'Dining at the Castaway Café',
+			url: 'https://www.astonmauikaanapalivillas.com/featured-amenities/',
+			imageUrl: 'https://s40340.pcdn.co/wp-content/uploads/2021/05/castaway-front-entrance-845x684-1-495x400.jpg'
+		},
+		{
+			name: 'Kaanapali Trolley',
+			url: 'https://www.astonmauikaanapalivillas.com/featured-amenities/',
+			imageUrl: 'https://s40340.pcdn.co/wp-content/uploads/2015/10/Kaanapali-Trolley-495x400.jpg'
+		},
+		{
+			name: 'Lobby Concierge by Pleasant Activities',
+			url: 'https://www.astonmauikaanapalivillas.com/featured-amenities/',
+			imageUrl: 'https://s40340.pcdn.co/wp-content/uploads/2016/02/K43A4789-495x400.jpg'
+		},
+		{
+			name: 'Fitness Center',
+			url: 'https://www.astonmauikaanapalivillas.com/featured-amenities/',
+			imageUrl: 'https://s40340.pcdn.co/wp-content/uploads/2021/06/Aston-Maui-Kaanapali-Villas-Fitness-Center-3_1440-495x400.jpg'
+		},
+		{
+			name: 'Spa La Mer',
+			url: 'https://www.astonmauikaanapalivillas.com/featured-amenities/',
+			imageUrl: 'https://s40340.pcdn.co/wp-content/uploads/2022/03/spa-la-mer-495x400-1.jpg'
+		},
+		{
+			name: 'On-site Yoga Class and Water Aerobics',
+			url: 'https://www.astonmauikaanapalivillas.com/featured-amenities/',
+			imageUrl: 'https://s40340.pcdn.co/wp-content/uploads/2021/09/In-House-Retreats-1-495x400.jpg'
+		}
+	],
+	roomGroupsArr: [
+		{
+			roomType: 'Hotel Room',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Hotel Room Deluxe',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Hotel Room Premium',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Studio Garden View',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Studio Garden View Deluxe',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Studio Garden View Premium',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Garden View',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Garden View Deluxe',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Garden View Premium',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Studio Ocean View',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Studio Ocean View Premium',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Studio Oceanside',
+			numberOfRooms: null,
+		},
+		{
+			roomType: 'Studio Oceanside Premium',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Ocean View',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Ocean View Premium',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Oceanside',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Oceanside Premium',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '2-Bedroom Suite Deluxe',
+			numberOfRooms: null,
+		},
+		{
+			roomType: '1-Bedroom Oceanfront Premium',
+			numberOfRooms: null,
+		}
+	],
+	currenciesAccepted: 'USD',
+	openingHours: '8:00am',
+	paymentAccepted: 'Credit Cards Are Accepted',
+	availableLanguage: 'English',
+	checkInTime: '16:00',
+	checkOutTime: '11:00',
+	imageArr: [
+		{
+			caption: '',
+			url: '',
+		},
+		{
+			caption: '',
+			url: '',
+		}
+	],
+	logoUrl: 'https://s40340.pcdn.co/wp-content/uploads/2016/08/MKV-logo_02.png',
+	slogan: 'A modern resort with old Hawaii at heart',
+	copyrightUrl: '',
+	facebookUrl: 'https://www.facebook.com/MauiKaanapaliVillas/',
+	twitterUrl: 'https://twitter.com/astonhotels',
+	instagramUrl: 'https://www.instagram.com/mauikaanapalivillas/?hl=en',
+	linkedinUrl: 'https://www.linkedin.com/company/aston-maui-kaanapali-villas',
+	contact: {
+		phone: '808-667-7791',
+		email: 'res.mkv@astonreservations.com',
+	},
+	location: {
+		street: '45 Kai Ala Drive',
+		city: 'Lahaina',
+		state: 'HI',
+		zip: '96761',
+		country: 'USA',
+	},
+	lowestPrice: 296,
+	highestPrice: 510,
+	rating: {
+		value: '4.4',
+		count: '1,206',
+	},
+	page: {
+		name: 'Home',
+		url: 'https://www.astonmauikaanapalivillas.com/',
+		video: {
+			name: 'Explore Our Resort',
+			description: 'Take a step back in time to experience true Hawaiian hospitality, the overwhelming sense of “aloha,” and the simple pleasures that can only be found at Aston Maui Kaanapali Villas. Our property rests on 11 epic acres of serene tropical gardens and Kaanapali’s immaculate white sand beach and our upgraded Premium Suites offer the finest luxury for a more indulgent experience. Here, there is no stress—only the sway of coconut palms and the pristine Pacific Ocean at your feet.',
+			thumbnailUrl: 'https://s40340.pcdn.co/wp-content/uploads/2023/09/Aston-Maui-Kaanapali-Villas-Pool-View_5040x3360-1500x430-1.jpg',
+			contentUrl: '',
+			embedUrl: '',
+			uploadDateISO: '2016-03-31T08:00:00+08:00',
+			duration: {
+				min: '1',
+				sec: '54',
+			}
+		},
+		inquireableItem: {
+			type: 'Hotel Room',
+			name: 'Room Name',
+		}
+	},
+	roomsArr: [
+		{
+			type: 'HotelRoom', // string -> HotelRoom or Suite
+			name: '', // string
+			url: '', // url
+			bookUrl: '', // url
+			tourUrl: '', // url
+			petsAllowedTrueOrFalse: true, // boolean
+			minPrice: '', // number
+			maxPrice: '', // number
+			description: '', // string
+			defaultPrice: '', // number
+			maxOccupancy: '', // number
+			checkInTime: '', // datetime
+			checkOutTime: '', // datetime
+			bedCount: '', // number
+			bedType: '', // string
+			smokingBoolean: true, // boolean
+			images: [
+				{
+					caption: '', // string
+					url: '', // url
+				},
+				{
+					caption: '', // string
+					url: '', // url
+				}
+			],
+			roomAmenitiesArr: [
+				{
+					name: '', // string
+					url: '', // url
+					imageUrl: '' // url
+				},
+				{
+					name: '', // string
+					url: '', // url
+					imageUrl: '' // url
+				},
+			],
+		}
+	],
+};
